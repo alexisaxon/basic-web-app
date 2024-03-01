d5f63726c30904011be94757d81f9e521ec77e5d
alexisaxon <aaxon@andrew.cmu.edu>
2024-03-01 17:09:14 -0500

    greatest

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -18,6 +18,16 @@ export default function QueryProcessor(query: string): string {
     const y = parseInt(match[2]);
     return ("" + x+y)
   }
+  const regex1 = /Which of the following numbers is the largest: (\d+), (\d+), (\d+)\?/i;
+  const match1 = query.match(regex);
+  
+  if (match1) {
+    // Extract the numbers from the matched groups
+    const x = parseInt(match1[1]);
+    const y = parseInt(match1[2]);
+    const z = parseInt(match1[3]);
+    return ("" + Math.max(x,y,z))
+  }
 
   return "";
 }
